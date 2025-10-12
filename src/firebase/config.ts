46b84b54d4ccd417ac808285496a7d270a36f57a
Pragathi P Kondaguli <pragathipkondaguli@gmail.com>
2025-10-12 10:47:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-626114644-3bb34",
+  "appId": "1:431043137373:web:9dd2d007e698c3548162b4",
+  "apiKey": "AIzaSyB0g2rLNXZA49NeDJt8ANV0VfB3Jp6Iw78",
+  "authDomain": "studio-626114644-3bb34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "431043137373"
+};
